60f05503d0d18eb80166abe71ef82b38c4335591
Spandan Pal <2spandan2@gmail.com>
2021-03-20 18:22:09 +0530

    added navbar in landing page

--- festcms2/settings.py
+++ festcms2/settings.py
@@ -22,15 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-# SECRET_KEY = config('SECRET_KEY')
-SECRET_KEY = "ssfsdfds"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = config('DEBUG',cast=bool)
-DEBUG = True
+DEBUG = config('DEBUG',cast=bool)
 
-# ALLOWED_HOSTS = config('ALLOWED_HOSTS',cast=Csv())
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = config('ALLOWED_HOSTS',cast=Csv())
 
 # Application definition
 
