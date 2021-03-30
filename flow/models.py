f31977b5495c15f7fca9e8950a6e02e12bef6472
Himanshu Shekhar <himanshu.shekharjha.272@gmail.com>
2021-03-31 03:09:47 +0530

    [Merge] Techmela page

--- flow/models.py
+++ flow/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator 
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.contrib.auth.models import User
 
 
@@ -9,43 +9,54 @@ class Timeline(models.Model):
     body = models.TextField()
     image = models.ImageField(blank=True, upload_to="day_img/")
 
+
 class Events(models.Model):
     eventDay = models.ForeignKey(Timeline, on_delete=models.DO_NOTHING)
-    eventName = models.CharField(max_length=50, unique=True, help_text="Enter the name of the event")
-    eventId = models.CharField(max_length=50,help_text="Enter the ID of the event")
+    eventName = models.CharField(
+        max_length=50, unique=True, help_text="Enter the name of the event")
+    eventId = models.CharField(
+        max_length=50, help_text="Enter the ID of the event")
     logo = models.ImageField(blank=True, upload_to="event_images/")
-    description = models.TextField(help_text="Enter short description of the event")
-    problem_statement = models.TextField(help_text="Enter the problem statement of the event here", blank=True)
+    description = models.TextField(
+        help_text="Enter short description of the event")
     date_time = models.DateTimeField()
-    venue = models.CharField(max_length=50,null=False, blank=True)
-    contact = models.CharField(max_length=50)
-    registration_open = models.BooleanField(default=True)
-    registration_link = models.CharField(max_length=50,blank=True,help_text="Enter the registration link here")
+    venue = models.CharField(max_length=50, null=False, blank=True)
+    registration_link = models.CharField(
+        max_length=50, blank=True, help_text="Enter the registration link here")
+    event_link = models.CharField(
+        max_length=50, blank=True, help_text="If there is no registration, then website link")
 
     def __str__(self):
         return self.eventName
 
+
 class Workshops(models.Model):
-    workshopName = models.CharField(max_length=100,unique=True, help_text="Enter name of workshop")
-    workshopId = models.CharField(max_length=50, help_text="Enter the ID of the workshop")
+    workshopName = models.CharField(
+        max_length=100, unique=True, help_text="Enter name of workshop")
+    workshopId = models.CharField(
+        max_length=50, help_text="Enter the ID of the workshop")
     logo = models.ImageField(blank=True, upload_to="workshop_images/")
-    description = models.TextField(help_text="Enter short description of the workshop")
+    description = models.TextField(
+        help_text="Enter short description of the workshop")
     date_time = models.DateTimeField()
     venue = models.CharField(max_length=50, null=False)
     contact = models.CharField(max_length=50)
     registration_open = models.BooleanField(default=True)
-    registration_link = models.CharField(max_length=50,blank=True,help_text="Enter the registration link here")
+    registration_link = models.CharField(
+        max_length=50, blank=True, help_text="Enter the registration link here")
 
     def __str__(self):
         return self.workshopName
+
 
 class Sponsors(models.Model):
     name = models.CharField(max_length=255)
     image = models.ImageField(blank=True, upload_to="sponsors/")
     url = models.EmailField(help_text='URL of the sponsor', blank=True)
-    
+
     def __str__(self):
-        return self.name 
+        return self.name
+
 
 class About(models.Model):
     text = models.TextField(help_text="Enter the about page of the fest here")
@@ -55,10 +66,11 @@ class About(models.Model):
     def __str__(self):
         return self.identifier
 
+
 class FAQ(models.Model):
-    identifier = models.CharField(max_length = 50)
-    question = models.CharField(max_length = 100)
-    answer = models.CharField(max_length = 100)
+    identifier = models.CharField(max_length=50)
+    question = models.CharField(max_length=100)
+    answer = models.CharField(max_length=100)
 
     class Meta:
         verbose_name = 'FAQ'
@@ -67,36 +79,45 @@ class FAQ(models.Model):
     def __str__(self):
         return self.identifier
 
+
 class MajorAttractions(models.Model):
     name = models.CharField(max_length=50, unique=True)
-    description = models.TextField(help_text="Enter the description of the attraction here")
-    logo = models.ImageField(blank = True, upload_to="mjr_attrs/")
+    description = models.TextField(
+        help_text="Enter the description of the attraction here")
+    logo = models.ImageField(blank=True, upload_to="mjr_attrs/")
 
     def __str__(self):
         return self.name
+
 
 class TeamCategory(models.Model):
-    name = models.CharField(max_length = 100, blank = False, help_text="Enter name of the team category here")
-    teamId = models.CharField(max_length = 50, unique=True, help_text="Enter category ID")
+    name = models.CharField(max_length=100, blank=False,
+                            help_text="Enter name of the team category here")
+    teamId = models.CharField(
+        max_length=50, unique=True, help_text="Enter category ID")
     team_image = models.ImageField(blank=True, upload_to="team_images/")
+
     def __str__(self):
         return self.name
+
 
 class TeamMember(models.Model):
     team = models.ManyToManyField(TeamCategory)
-    name = models.CharField(max_length=50, unique=True, help_text="Enter the name of the member here")
-    position = models.CharField(max_length=50, help_text="Enter the position of the person", blank=True)
-    profile_img = models.ImageField(blank= True, upload_to="members/")
+    name = models.CharField(max_length=50, unique=True,
+                            help_text="Enter the name of the member here")
+    position = models.CharField(
+        max_length=50, help_text="Enter the position of the person", blank=True)
+    profile_img = models.ImageField(blank=True, upload_to="members/")
     club_choice = (
-        ("GLUG","GNU/Linux Users' Group"),
-        ("CCA","Centre for Cognitive Activities"),
-        ("SAE","SAE"),
-        ("MNTC","Maths n Tech Club"),
-        ("REC","RECursion"),
+        ("GLUG", "GNU/Linux Users' Group"),
+        ("CCA", "Centre for Cognitive Activities"),
+        ("SAE", "SAE"),
+        ("MNTC", "Maths n Tech Club"),
+        ("REC", "RECursion"),
     )
     choice = models.CharField(
         max_length=5,
-        choices= club_choice,
+        choices=club_choice,
         default="GLUG"
     )
 
@@ -105,19 +126,34 @@ class TeamMember(models.Model):
 
 
 class TechmelaProject(models.Model):
-    name = models.CharField(max_length=30)
+    title = models.CharField(max_length=30, default="Title here")
     team_name = models.CharField(max_length=20)
+    description = models.TextField(default="Description here")
+    pdf_link = models.URLField(default="https://drive.google.com/file/d/0B1HXnM1lBuoqMzVhZjcwNTAtZWI5OS00ZDg3LWEyMzktNzZmYWY2Y2NhNWQx/preview")
+    video_link = models.URLField(
+        default="https://drive.google.com/file/d/1KFFhLqRLhdF27KwVVe0npoVX0lNLPhpM/preview")
+    project_domain_choice = (
+        ('Robotics', 'Robotics'),
+        ('Assistive Technology', 'Assistive Technology'),
+        ('Software Projects', 'Software Projects'),
+    )
+    domain = models.CharField(
+        max_length=30, choices=project_domain_choice, default='Robotics')
+    project_image = models.ImageField(upload_to="techmela_projects/", null=True, blank=True)
 
     def __str__(self):
-        return (self.name + " by Team:" + self.team_name)
+        return (self.title + " by Team:" + self.team_name)
 
 
 class Review(models.Model):
     project = models.ForeignKey(TechmelaProject, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    mark = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(10), MinValueValidator(0)])
-    feas = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(10), MinValueValidator(0)])
-    tech = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(10), MinValueValidator(0)])
+    mark = models.PositiveIntegerField(
+        default=0, validators=[MaxValueValidator(10), MinValueValidator(0)])
+    feas = models.PositiveIntegerField(
+        default=0, validators=[MaxValueValidator(10), MinValueValidator(0)])
+    tech = models.PositiveIntegerField(
+        default=0, validators=[MaxValueValidator(10), MinValueValidator(0)])
 
     def __str__(self):
         return ("Review of " + self.project.name + " by " + self.user.username)
--- flow/views.py
+++ flow/views.py
@@ -8,14 +8,17 @@ from django.contrib.auth.models import User
 from .models import *
 # Create your views here.
 
+
 def index(request):
     workshops = Workshops.objects.all()
     faqs = FAQ.objects.all()
     about = About.objects.all()
     sponsors = Sponsors.objects.all()
     attractions = MajorAttractions.objects.all()
-    context = {'workshops': workshops, 'faqs': faqs, 'about':about, 'sponsors':sponsors, 'attractions': attractions}
+    context = {'workshops': workshops, 'faqs': faqs, 'about': about,
+               'sponsors': sponsors, 'attractions': attractions}
     return render(request, 'flow/index.html', context)
+
 
 def home(request):
     workshops = Workshops.objects.all()
@@ -23,9 +26,11 @@ def home(request):
     about = About.objects.all()
     sponsors = Sponsors.objects.all()
     attractions = MajorAttractions.objects.all()
-    context = {'events': events, 'workshops': workshops, 'faqs': faqs, 'about':about, 'sponsors':sponsors, 'attractions': attractions}
+    context = {'events': events, 'workshops': workshops, 'faqs': faqs,
+               'about': about, 'sponsors': sponsors, 'attractions': attractions}
     return render(request, 'flow/home.html', context)
-    
+
+
 def events(request, id):
     context = {}
     try:
@@ -33,59 +38,72 @@ def events(request, id):
         events = Events.objects.filter(eventGroup=categrory)
         context['events'] = events
         context['group'] = categrory
-        return render(request,'flow/events.html',context)
+        return render(request, 'flow/events.html', context)
     except ObjectDoesNotExist:
         raise Http404
 
+
 def workshops(request):
     workshops = Workshops.objects.all()
-    context = {'workshops':workshops}
-    return render(request,"flow/workshops.html",context)
+    context = {'workshops': workshops}
+    return render(request, "flow/workshops.html", context)
+
 
 def sponsor_view(request):
     sponsors = Sponsors.objects.all()
-    context = {'sponsors':sponsors}
-    return render(request,'flow/sponsors', context)
+    context = {'sponsors': sponsors}
+    return render(request, 'flow/sponsors', context)
+
 
 def about_page(request):
     about = About.objects.all()
-    context = {'about':about}
-    return render(request,'flow/aboutus.html',context)
+    context = {'about': about}
+    return render(request, 'flow/aboutus.html', context)
+
+
 def contactus(request):
-    return render(request,'flow/contactus.html')
+    return render(request, 'flow/contactus.html')
+
+
 def faq_page(request):
     faqs = FAQ.objects.all()
-    context = {'faqs':faqs}
-    return render(request,"flow/faq.html",context)
+    context = {'faqs': faqs}
+    return render(request, "flow/faq.html", context)
+
 
 def attractions_page(request):
     attractions = MajorAttractions.objects.all()
-    context = {'attractions':attractions}
-    return render(request,'flow/attractions.html',context)
+    context = {'attractions': attractions}
+    return render(request, 'flow/attractions.html', context)
+
 
 def team_page(request):
     umbrellas = TeamCategory.objects.all()
     members = []
     for umbrella in umbrellas:
         members.append((TeamMember.objects.filter(team=umbrella).values()))
-    umbrellas=TeamCategory.objects.values()
-    umbrellas=[umbrella for umbrella in umbrellas]
-    members_list=[]
+    umbrellas = TeamCategory.objects.values()
+    umbrellas = [umbrella for umbrella in umbrellas]
+    members_list = []
     for category in members:
-        category_members=[member for member in category]
+        category_members = [member for member in category]
         members_list.append(category_members)
-    context = {'teams':umbrellas,'members':members_list}
-    return render(request,"flow/teampage.html", context)
+    context = {'teams': umbrellas, 'members': members_list}
+    return render(request, "flow/teampage.html", context)
+
 
 def timeline(request):
     days = Timeline.objects.all()
     return render(request, 'flow/timeline2.html', {'days': days})
 
+
 def timeline_detail(request, id):
     day = Timeline.objects.get(day_number=id)
-    events = Events.objects.filter(eventDay__day_number__exact=id).order_by('date_time')
+    events = Events.objects.filter(
+        eventDay__day_number__exact=id).order_by('date_time')
     context = {'day': day, 'events': events}
     return render(request, 'flow/event_timeline2.html', context)
+
 
 def signin(request):
     if request.user.is_authenticated:
@@ -102,6 +120,7 @@ def signin(request):
     else:
         return render(request, 'flow/signin.html', {'msg': None})
 
+
 def judge_projects(request):
     context = {}
     context['msg'] = ""
@@ -117,7 +136,8 @@ def judge_projects(request):
             t = request.POST['tech']
             project_id = request.POST['project']
             project = TechmelaProject.objects.get(id=project_id)
-            review = Review(project=project, user=request.user, mark=m, feas=f, tech=t)
+            review = Review(project=project, user=request.user,
+                            mark=m, feas=f, tech=t)
             try:
                 review.save()
                 context["msg"] = "Saved last entry"
@@ -132,12 +152,13 @@ def judge_projects(request):
                 setattr(context['projects'][idx], 'show', False)
     return render(request, 'flow/projects.html', context)
 
+
 def view_reviews(request):
     context = {}
     if request.user.is_authenticated:
         projects = TechmelaProject.objects.all()
         context["projects"] = projects
-        for idx,project in enumerate(context["projects"]):
+        for idx, project in enumerate(context["projects"]):
             reviews = Review.objects.all().filter(project=project)
             grand_total = 0
             avg_mark = 0
@@ -171,5 +192,10 @@ def view_reviews(request):
         print(context)
     return render(request, 'flow/reviews.html', context)
 
+
 def techmela(request):
-    return render(request, "flow/techmela.html")
+    robotics = TechmelaProject.objects.filter(domain="Robotics")
+    assistive = TechmelaProject.objects.filter(domain="Assistive Technology")
+    software = TechmelaProject.objects.filter(domain="Software Projects")
+
+    return render(request, "flow/techmela.html", {'robotics': robotics, 'assistive': assistive, 'software' : software})
