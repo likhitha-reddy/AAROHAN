0da0b8efb960c1ed1e9c297755b122aa7416f842
Ayush Pratap Singh <apsingh1843@gmail.com>
2022-02-24 12:50:27 +0530

    landing page

--- flow/static/js/home.js
+++ flow/static/js/home.js
@@ -1,94 +1,41 @@
-// for mobile workshop section carousel
-;(function () {
-  'use strict'
+const VIDEO = document.getElementById('video');
+const offsetCloseBtnHidden = document.getElementById('hiddenBtn');
+const offsetShowBtnHidden = document.getElementById('shownBtn');
 
-  var carousel = document.getElementsByClassName('carousel')[0],
-    slider = carousel.getElementsByClassName('carousel__slider')[0],
-    items = carousel.getElementsByClassName('carousel__slider__item'),
-    prevBtn = carousel.getElementsByClassName('carousel__prev')[0],
-    nextBtn = carousel.getElementsByClassName('carousel__next')[0]
-
-  var width,
-    height,
-    totalWidth,
-    margin = 20,
-    currIndex = 0,
-    interval,
-    intervalTime = 2000
-
-  function init () {
-    resize()
-    // move(Math.floor(items.length / 2));
-    move(1)
-    bindEvents()
-
-    timer()
-  }
-
-  function resize () {
-    // width = Math.max(window.innerWidth * .25, 275),
-    // height = window.innerHeight * .5,
-    ;(width = 200), (height = 180), (totalWidth = width * items.length)
-
-    slider.style.width = totalWidth + 'px'
-
-    for (var i = 0; i < items.length; i++) {
-      let item = items[i]
-      item.style.width = width - margin * 2 + 'px'
-      item.style.height = height + 'px'
+// VIDEO.onloadeddata = function() {
+//     console.log("Video loaded");
+//     setTimeout(() => {
+//         loadend();
+//     }, 2000);
+// };
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        console.log("loaded");
+        setTimeout(() => {
+            loadend();
+        }, 2000);
+    } else {
+        console.log("not loaded");
+        setTimeout(() => {
+            loadend();
+        }, 2000);
     }
-  }
+};
 
-  function move (index) {
-    if (index < 1) index = items.length
-    if (index > items.length) index = 1
-    currIndex = index
-
-    for (var i = 0; i < items.length; i++) {
-      let item = items[i],
-        box = item.getElementsByClassName('item__3d-frame')[0]
-      if (i == index - 1) {
-        item.classList.add('carousel__slider__item--active')
-        box.style.transform = 'perspective(1200px)'
-      } else {
-        item.classList.remove('carousel__slider__item--active')
-        box.style.transform =
-          'perspective(1200px) rotateY(' + (i < index - 1 ? 40 : -40) + 'deg)'
-      }
-    }
-
-    slider.style.transform =
-      'translate3d(' +
-      (index * -width + width / 2 + window.innerWidth / 2) +
-      'px, 0, 0)'
-  }
-
-  function timer () {
-    clearInterval(interval)
-    interval = setInterval(() => {
-      move(++currIndex)
-    }, intervalTime)
-  }
-
-  function prev () {
-    move(--currIndex)
-    timer()
-  }
-
-  function next () {
-    move(++currIndex)
-    timer()
-  }
-
-  function bindEvents () {
-    window.onresize = resize
-    prevBtn.addEventListener('click', () => {
-      prev()
-    })
-    nextBtn.addEventListener('click', () => {
-      next()
-    })
-  }
-
-  init()
-})()
+function closeAfterSometimePC() {
+    document.getElementById('pcham').classList.toggle('active')
+    setTimeout(() => {
+        offsetCloseBtnHidden.click()
+        document.getElementById('pcham').classList.toggle('active')
+    }, 800);
+}
+function closeAfterSometimeMob(){
+    document.getElementById('ocanva').classList.toggle('active')
+    // setTimeout(() => {
+        offsetShowBtnHidden.click()
+    // }, 650);
+}
+function loadend() {
+    document.getElementById('loading').style.display= "none";
+    document.getElementById('root').style.display= "block";
+}
